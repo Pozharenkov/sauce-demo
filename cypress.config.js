d9c24c2116685a156e3c11ff2ed4e9ca6b754b3f
Pozharenkov <dimitar.pozharenkov@gmail.com>
2026-07-08 12:10:56 +0200

    Update cypress config and dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -5,17 +5,17 @@ const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.feature",
+    baseUrl: "https://www.saucedemo.com",
+    specPattern: "cypress/e2e/features/**/*.feature",
+    supportFile: "cypress/support/e2e.js",
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
-
       on(
         "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
         })
       );
-
       return config;
     },
   },
